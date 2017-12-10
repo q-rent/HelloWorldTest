aaafd70300c185f36e34e2fb62f9ed1f44b07d4f
kharya0 <kkdvillegas@slu.edu.ph>
2017-12-10 16:06:06 +0800

    Added more nonsense

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ import java.lang.*;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("...Hello World!");
 		System.out.println("Hi din");
 		System.out.println("Test");
 		System.out.println(""1+1);
